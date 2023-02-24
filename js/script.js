986cd4d3dcce535db0b5f6daea6d60e6512e168e
barszczwiktoria <barszcz.wiktoria@gmail.com>
2023-02-24 13:26:09 +0100

    ES6+ features added

--- js/script.js
+++ js/script.js
@@ -1,68 +1,62 @@
-let formElement = document.querySelector(".js-form");
-let amountElement = document.querySelector(".js-amount");
-let exchangeElement = document.querySelector(".js-exchange");
-let resultElement = document.querySelector(".js-result");
-
-let rateEUR = 4.39;
-let rateUSD = 4.69;
-let rateMKD = 0.076;
-let ratePLNEUR = 0.21;
-let ratePLNUSD = 0.23;
-let ratePLNMKD = 13.18;
-
-formElement.addEventListener("reset", () => {
-    resultElement.innerText = "";
-});
-
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    let amount = amountElement.value;
-    let exchange = exchangeElement.value;
-    let result = resultElement.value;
-    
-
-    switch (exchange) {
-        case "EUR-PLN":
-            result = amount * rateEUR
-            break;
-
-        case "USD-PLN":
-            result = amount * rateUSD
-            break;
-
-        case "MKD-PLN":
-            result = amount * rateMKD
-            break;
-
-        case "PLN-EUR":
-            result = amount * ratePLNEUR
-            break;
-
-        case "PLN-USD":
-            result = amount * ratePLNUSD
-            break;
-
-        case "PLN-MKD":
-            result = amount * ratePLNMKD
-            break;
+{
+    const welcome = () => {
+        console.log("hello everyone in my second project - currency calclulator");
     }
 
-    resultElement.innerText = (` ${result.toFixed(2)} ${exchange.split('-')[1]}`);
-})
+    welcome()
 
+    const calculateResult = (amount, exchange) => {
+        const rateEUR = 4.39;
+        const rateUSD = 4.69;
+        const rateMKD = 0.076;
+        const ratePLNEUR = 0.21;
+        const ratePLNUSD = 0.23;
+        const ratePLNMKD = 13.18;
 
+        switch (exchange) {
+            case "EUR-PLN":
+               return amount * rateEUR;
+            case "USD-PLN":
+                return  amount * rateUSD;
+            case "MKD-PLN":
+                return  amount * rateMKD;
+            case "PLN-EUR":
+                return  amount * ratePLNEUR;
+            case "PLN-USD":
+                return  amount * ratePLNUSD;
+            case "PLN-MKD":
+                return  amount * ratePLNMKD;
+        }
+    };
 
+    const updateResultText = (result, exchange) => {
+        const resultElement = document.querySelector(".js-result");
+      
+        resultElement.innerText = (`${result.toFixed(2)} ${exchange.split('-')[1]}`);
+    }      
 
+    const onFormSubmit = (event) => {
+        event.preventDefault();
 
+        const amountElement = document.querySelector(".js-amount");
+        const exchangeElement = document.querySelector(".js-exchange");
 
+        const amount = +amountElement.value;
+        const exchange = exchangeElement.value;
 
+        const result = calculateResult(amount, exchange);
 
+        updateResultText(result, exchange);
+    };
+ 
 
+    const init = () => {
+        const formElement = document.querySelector(".js-form");
 
+        formElement.addEventListener("submit", onFormSubmit);
 
+    };
 
+    init();
 
-
-
-
+    }
